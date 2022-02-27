fb3431f0bcad40606dfb72fc61eb47f9c1ce85b8
lore <751029317@qq.com>
2022-02-27 20:04:43 +0800

    footer (fin）

--- src/App.jsx
+++ src/App.jsx
@@ -28,13 +28,25 @@ export default class App extends Component {
     this.setState({todos: updateTodo});
   }
 
+  selectAll = (checked) => {
+    const {todos} = this.state;
+    const selectAll = todos.map(todo => {todo.checked = checked; return todo;});
+    this.setState({todos: selectAll});
+  }
+
+  clearAllDone = () => {
+    const {todos} = this.state;
+    const clearAllDone = todos.filter(todo => !todo.checked);
+    this.setState({todos: clearAllDone});
+  }
+
   render() {
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Add addItem={this.addItem} />
           <List todos={this.state.todos} deleteItem={this.deleteItem} update={this.update}/>
-          <Footer todos={this.state.todos} />
+          <Footer todos={this.state.todos} selectAll={this.selectAll} clearAllDone={this.clearAllDone} />
         </div>
       </div>
     )
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,10 +3,17 @@ import './index.css';
 
 export default class Footer extends Component {
 
-	handleClear = () => {
+	handleClearDone = () => {
 		// if checked --> delete
-		
+		this.props.clearAllDone();
 	}
+
+	handleSelectAll = (event) => {
+		// todos checked 
+		const {checked} = event.target;
+		this.props.selectAll(checked);
+	}
+
 
 	render() {
 		const {todos} = this.props;
@@ -14,13 +21,14 @@ export default class Footer extends Component {
 		return (
 			<div className="todo-footer">
 				<label>
-					<input type="checkbox" />
+					<input type="checkbox" checked={(todos.length !== 0 && todos.length === finished)? true: false} 
+					onChange={this.handleSelectAll} />
 				</label>
 				<span>
 					<span>已完成 {finished} </span> / 全部 {todos.length}
 				</span>
 				<button className="btn btn-danger" 
-						onClick={this.handleClear}
+						onClick={this.handleClearDone}
 				>清除已完成任务</button>
 			</div>
 		)
